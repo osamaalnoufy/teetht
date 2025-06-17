8b6b5997ef30b76539a485bbf8a735f6618875a5
osamaalnoufy <osamaalnoufy0gmail.com>
2025-06-17 13:00:02 +0300

    osama7

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ dotenv.config();
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
       }),
     }),
     CloudinaryModule,
